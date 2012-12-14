2c3c8ffe50bc63028ff70469166a6def1fa15718
Will Tinsdeall <ivebeenlinuxed@googlemail.com>
2012-12-14 03:11:32 +0000

    Added placeholder for ported documentation

---
+++ framework/application/controller/Docs.php
@@ -0,0 +1,22 @@
+<?php
+namespace Controller;
+
+class Docs extends BaseController {
+	/**
+	 * Display documentation on API
+	 * 
+	 * TODO Port api.php page
+	 */
+	public function api() {
+		
+	}
+	
+	/**
+	 * Display information on alternative services
+	 *
+	 * TODO Port alternatives.php page
+	 */
+	public function alternatives() {
+	
+	}
+}
